926fbfc0158a4fd9fd222f82d60a62d36889adf7
ahmedAgawad <ahmed.m.elshennawi@gmail.com>
2024-09-10 03:40:29 +0300

    course2mod3 finding a gene and web links part 4 done

---
+++ course2-solving-problems-with-software/module3/finding-a-gene-and-web-links/Part 4: Finding Web Links/Part4.java
@@ -0,0 +1,22 @@
+import edu.duke.*;
+/**
+ * Write a description of Part4 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Part4 {
+public void webLinks() {
+  URLResource ur = new URLResource("https://www.dukelearntoprogram.com/course2/data/manylinks.html");
+  for(String line : ur.lines()) {
+    String originalStr = line;
+    String loweredStr = line.toLowerCase();
+    if(loweredStr.indexOf("youtube.com") != -1) {
+       int startIndex = originalStr.indexOf("\"");
+       int endIndex = originalStr.lastIndexOf("\"");
+       String linkOnly = originalStr.substring(startIndex, endIndex + 1);
+       System.out.println(linkOnly);
+    } 
+  }
+}
+}
