d3c4ad36d764492ef6e8f4d72ebbdea7c0d8ff8c
ahmedAgawad <ahmed.m.elshennawi@gmail.com>
2024-09-21 22:04:28 +0300

    course3mod3 : Improving GladLibs Assignment 2: Words in Files done

---
+++ course3-arrays-lists-structured-data/module 3/using-and-improving-gladlibs/improving-gladlibs/Assignment 2: Words in Files/WordsInFiles.java
@@ -0,0 +1,103 @@
+import edu.duke.*;
+import java.util.*;
+import java.io.*;
+/**
+ * Write a description of WordsInFiles here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class WordsInFiles {
+private HashMap<String, ArrayList<String>> wordsToFiles;
+
+public WordsInFiles() {
+     wordsToFiles = new HashMap<String, ArrayList<String>>();
+}
+
+private void addWordsFromFile(File f) {
+    FileResource fr = new FileResource(f);
+    for(String word : fr.words()) {
+        if(!wordsToFiles.containsKey(word)) {
+            ArrayList<String> list = new ArrayList<String>();
+            list.add(f.getName());            
+            wordsToFiles.put(word, list);
+        } else {
+            ArrayList<String> existedList = wordsToFiles.get(word);
+            if(!existedList.contains(f.getName())) {
+                existedList.add(f.getName());
+            }
+            wordsToFiles.put(word, existedList);
+        }
+    }
+}
+
+public void buildWordFileMap() {
+    wordsToFiles.clear();
+    DirectoryResource dr = new DirectoryResource();
+    for(File eachFile : dr.selectedFiles()) {
+        addWordsFromFile(eachFile);
+    }
+}
+
+public int maxNumber() {
+    int maxNumber = 0;
+    
+    for(String keyName : wordsToFiles.keySet()) {
+        int currentWordNumber = wordsToFiles.get(keyName).size();
+        if(currentWordNumber > maxNumber) {
+            maxNumber = currentWordNumber;
+        }
+    }
+    
+    return maxNumber;
+}
+
+
+
+public ArrayList wordsInNumFiles(int number) {
+    ArrayList<String> wordsList = new ArrayList<String>();
+    
+    for(String keyName : wordsToFiles.keySet()) {
+         int currentWordNumber = wordsToFiles.get(keyName).size();
+         if(currentWordNumber == number) {
+             wordsList.add(keyName);       
+         }
+    }
+    
+    return wordsList;
+}
+
+
+public void printFilesIn(String word) {
+    for(String keyName : wordsToFiles.keySet()) {
+        if(keyName.equals(word)) {
+            ArrayList<String> fileNames = wordsToFiles.get(keyName);
+            
+            for(int i = 0; i < fileNames.size() ;i++) {
+                System.out.println(fileNames.get(i));
+            }
+        }
+    }
+}
+
+public void tester() {
+    buildWordFileMap();
+    int maxNum = maxNumber();
+    for(String keyName : wordsToFiles.keySet()) {
+        System.out.println(keyName);
+        ArrayList<String> fileNames = wordsToFiles.get(keyName);
+        for(int i = 0; i < fileNames.size(); i++) {
+            System.out.print(fileNames.get(i) + "  ");
+        }
+        System.out.println("\n");
+    }
+    System.out.println(maxNum);
+    ArrayList<String> wordsList = wordsInNumFiles(2);
+    for(int i =0; i < wordsList.size(); i++) {
+        System.out.print(wordsList.get(i) + " ");
+    }
+    System.out.println("\n");
+    printFilesIn("dogs");
+} 
+
+}
