bef3eb97b5f518698208d6f9eef836a9b9988330
ahmedAgawad <ahmed.m.elshennawi@gmail.com>
2024-09-26 02:49:53 +0300

    course3mod5 : Unknown Language, Unknown Key Length implementation done (tested)

--- course3-arrays-lists-structured-data/module 5/VigenereBreaker.java
+++ course3-arrays-lists-structured-data/module 5/VigenereBreaker.java
@@ -1,6 +1,6 @@
 import java.util.*;
 import edu.duke.*;
-
+import java.io.*;
 public class VigenereBreaker {
     public String sliceString(String message, int whichSlice, int totalSlices) {
         StringBuilder sb = new StringBuilder();
@@ -49,36 +49,87 @@ public class VigenereBreaker {
     
     public String breakForLanguage(String message, HashSet<String> dictionary) {
         int max = -99999;
+        int keyLength = 0;
         String correctDecrypted = "";
+        char mostCommonInDic = mostCommonCharIn(dictionary);
         for(int i = 1; i <= 100; i++) {
-            int [] keys = tryKeyLength(message, i, 'e');
+            int [] keys = tryKeyLength(message, i, mostCommonInDic);
             VigenereCipher vc = new VigenereCipher(keys);
             String decrypted = vc.decrypt(message);
-            String [] words = decrypted.split("\\W+");
-            int correctWords = 0;
-            for(int j = 0; j < words.length; j++) {
-                if(dictionary.contains(words[j].toLowerCase())) {
-                    correctWords++;
-                }
-            }
             
+            int correctWords = countWords(decrypted, dictionary);
+        
             if(correctWords > max) {
                 max = correctWords;
                 correctDecrypted = decrypted;
+                keyLength = i;
             }
         }
+
         
+
         return correctDecrypted;
     }
     
+    
+    public char mostCommonCharIn(HashSet<String> dictionary) {
+        String chars = "abcdefghijklmnopqrstuvwxyz";
+        int [] counts = new int[26];
+    
+        for(String word : dictionary) {
+            for(int i = 0; i < word.length(); i++) {
+                int currentCharIdx = chars.indexOf(word.charAt(i));
+                if(currentCharIdx != -1) {
+                     counts[currentCharIdx]++; 
+                }
+            }            
+        }
+     
+        
+        int maxIdx = 0;
+        
+        for(int i = 1; i < counts.length; i++) {
+            if(counts[i] > counts[maxIdx]) {
+                maxIdx = i;
+            }
+        }
+        
+        return chars.charAt(maxIdx);
+    }
+    
+    public String breakForAllLangs(String encrypted, HashMap<String, HashSet<String>> dics) {
+        String langForDecryption = "";
+        int maxNoCorrect = 0;
+        String decrypted = "";
+        for(String lang : dics.keySet()) {
+            HashSet<String> currentDic = dics.get(lang);
+            String currentDicDecrypted = breakForLanguage(encrypted, currentDic);
+            int currentDecryptedCount = countWords(currentDicDecrypted, currentDic);
+            if(currentDecryptedCount > maxNoCorrect) {
+                decrypted = currentDicDecrypted;
+                maxNoCorrect = currentDecryptedCount;
+                langForDecryption = lang;
+            }
+        }
+        
+        System.out.println(decrypted);
+        System.out.println(langForDecryption);
+        return decrypted;
+    }
+    
+  
     public void breakVigenere () {
         FileResource fr = new FileResource();
         String message = fr.asString();
-        FileResource dicFile = new FileResource("dictionaries/English");
-        HashSet<String> dic = readDictionary(dicFile);
+        HashMap<String, HashSet<String>> langToDics = new HashMap<String,HashSet<String>>();
+        DirectoryResource dr = new DirectoryResource();
+        for(File f : dr.selectedFiles()) {
+            FileResource currentDicFile = new FileResource(f);
+            HashSet<String> currentLangDic = readDictionary(currentDicFile);
+            langToDics.put(f.getName() , currentLangDic);
+        }
+        breakForAllLangs(message, langToDics);
         
-        String decrypted = breakForLanguage(message, dic);
-        System.out.println(decrypted);
     }
     
 }
