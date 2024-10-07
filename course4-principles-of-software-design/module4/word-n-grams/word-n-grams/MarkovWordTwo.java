5fa5ae6590cc9cdd28aadbeb7507e7693e463b16
ahmedAgawad <ahmed.m.elshennawi@gmail.com>
2024-10-07 15:51:14 +0300

    course4mod4 : Words N-Grams Assignment 1 & 2 done

---
+++ course4-principles-of-software-design/module4/word-n-grams/word-n-grams/MarkovWordTwo.java
@@ -0,0 +1,82 @@
+
+/**
+ * Write a description of MarkovWordTwo here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+import java.util.*;
+
+public class MarkovWordTwo implements IMarkovModel {
+private String[] myText;
+    private Random myRandom;
+    
+    public MarkovWordTwo() {
+        myRandom = new Random();
+    }
+    
+    public void setRandom(int seed) {
+        myRandom = new Random(seed);
+    }
+    
+    public void setTraining(String text){
+        myText = text.split("\\s+");
+    }
+    
+    public String getRandomText(int numWords){
+        StringBuilder sb = new StringBuilder();
+        int index = myRandom.nextInt(myText.length-2);  // random word to start with
+        String key1 = myText[index];
+        String key2 = myText[index + 1];
+        sb.append(key1);
+        sb.append(" ");
+        sb.append(key2);
+        sb.append(" ");
+        
+        for(int k=0; k < numWords-2; k++){
+            ArrayList<String> follows = getFollows(key1, key2);
+            if (follows.size() == 0) {
+                break;
+            }
+            //System.out.println(key + "  " + follows);
+            index = myRandom.nextInt(follows.size());
+            String next = follows.get(index);
+            sb.append(next);
+            sb.append(" ");
+            key1 = key2;
+            key2 = next;
+        }
+        
+        return sb.toString().trim();
+    }
+    
+    private int indexOf(String [] words, String target1, String target2, int start) {
+        for(int i = start; i < words.length - 1; i++) {
+            if(words[i].equals(target1) && words[i + 1].equals(target2) ) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    
+    private ArrayList<String> getFollows(String key1, String key2) {
+        ArrayList<String> follows = new ArrayList<String>();
+        int startPos = 0;
+        
+        while (true) {
+            int index = indexOf(myText, key1, key2, startPos);
+            
+            if (index == -1 || index + 2 >= myText.length) {
+                break;
+            }
+            
+            follows.add(myText[index + 2]);
+            startPos = index + 2;
+        }
+        
+        return follows;
+    }
+    
+    
+    
+}
